89048113eb1a1497376e6ec4b46b19581b68cb89
Ketlyn Sena <ketlynsena@outlook.com>
2019-11-03 18:57:48 -0300

    commit #4

--- Money.java
+++ Money.java
@@ -1,20 +1,26 @@
 abstract class Money{
 
     protected int amount;
+    abstract Money times(int multiplier);
+    abstract String currency;
+ 
+
+    static Money dollar(int amount){
+        return new Dollar(amount, "USD");
+    }
+
+    static Money franc(int amount){
+        return new Franc(amount, "CHF");
+    }
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
-
-    static Money dollar(int amount){
-        return new Dollar(amount);
-    }
-
-    static Money franc(int amount){
-        return new Franc(amount);
-    }
-      
-    abstract Money times(int multiplier);
 
 }
